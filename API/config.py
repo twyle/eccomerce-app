4fae70c58c0edb5c1076932e4033490c7355aa81
lyleokoth <okothlyleochieng@gmail.com>
2022-04-26 19:22:45 +0300

    feat: added the unut and intergration test.

--- API/config.py
+++ API/config.py
@@ -14,19 +14,18 @@ class BaseConfig():
     TESTING = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    MONGODB_CONNECT = True
-
 class TestingConfig(BaseConfig):
     """Empty class docstring"""
 
     DEBUG = True
     TESTING = True
 
-    MONGODB_DB = os.environ['MONGODB_DB_TEST']
-    MONGODB_HOST = os.environ['MONGODB_HOST_TEST']
-    MONGODB_PORT = os.environ['MONGODB_PORT_TEST']
-    MONGODB_USERNAME = os.environ['MONGODB_USERNAME_TEST']
-    MONGODB_PASSWORD = os.environ['MONGODB_PASSWORD_TEST']
+    MONGODB_DB = 'TEST_DB'
+    MONGODB_HOST = '127.0.0.1'
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = ''
+    MONGODB_PASSWORD = ''
+    MONGODB_CONNECT = True
 
 
 class DevelopmentConfig(BaseConfig):
@@ -37,11 +36,12 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = False
 
-    MONGODB_DB = os.environ['MONGODB_DB_DEV']
-    MONGODB_HOST = os.environ['MONGODB_HOST_DEV']
-    MONGODB_PORT = os.environ['MONGODB_PORT_DEV']
-    MONGODB_USERNAME = os.environ['MONGODB_USERNAME_DEV']
-    MONGODB_PASSWORD = os.environ['MONGODB_PASSWORD_DEV']
+    MONGODB_DB = 'flask-react-blog-simple-dev'
+    MONGODB_HOST = '127.0.0.1'
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = ''
+    MONGODB_PASSWORD = ''
+    MONGODB_CONNECT = True
 
 
 class ProductionConfig(BaseConfig):
@@ -52,8 +52,9 @@ class ProductionConfig(BaseConfig):
     DEBUG = False
     TESTING = False
 
-    MONGODB_DB = os.environ['MONGODB_DB_PROD']
-    MONGODB_HOST = os.environ['MONGODB_HOST_PROD']
-    MONGODB_PORT = os.environ['MONGODB_PORT_PROD']
-    MONGODB_USERNAME = os.environ['MONGODB_USERNAME_PROD']
-    MONGODB_PASSWORD = os.environ['MONGODB_PASSWORD_PROD']
+    MONGODB_DB = 'flask-react-blog-simple-prod'
+    MONGODB_HOST = 'mongodb+srv://cluster0.ctzy7.mongodb.net/'
+    MONGODB_PORT = 27017
+    MONGODB_USERNAME = 'flask-api'
+    MONGODB_PASSWORD = 'flask-api'
+    MONGODB_CONNECT = True
