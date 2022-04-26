b0fc35ed2c4bd948c18307fa8bfbc90a145f8a7d
lyleokoth <okothlyleochieng@gmail.com>
2022-04-26 17:38:13 +0300

    fix: fixed the FLASK_ENV error.

--- API/config.py
+++ API/config.py
@@ -14,18 +14,19 @@ class BaseConfig():
     TESTING = False
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+    MONGODB_CONNECT = True
+
 class TestingConfig(BaseConfig):
     """Empty class docstring"""
 
     DEBUG = True
     TESTING = True
 
-    MONGODB_DB = 'TEST_DB'
-    MONGODB_HOST = '127.0.0.1'
-    MONGODB_PORT = 27017
-    MONGODB_USERNAME = ''
-    MONGODB_PASSWORD = ''
-    MONGODB_CONNECT = True
+    MONGODB_DB = os.environ['MONGODB_DB']
+    MONGODB_HOST = os.environ['MONGODB_HOST']
+    MONGODB_PORT = os.environ['MONGODB_PORT']
+    MONGODB_USERNAME = os.environ['MONGODB_USERNAME']
+    MONGODB_PASSWORD = os.environ['MONGODB_PASSWORD']
 
 
 class DevelopmentConfig(BaseConfig):
